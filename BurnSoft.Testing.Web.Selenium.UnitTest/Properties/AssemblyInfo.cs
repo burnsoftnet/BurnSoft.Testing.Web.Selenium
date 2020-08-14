699b1a661eb2b1fb4b16e868f24c5bbb89744673
Joe M <joe.mireles@burnsoft.net>
2020-08-14 18:46:39 -0400

    added unit test

---
+++ BurnSoft.Testing.Web.Selenium.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BurnSoft.Testing.Web.Selenium.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BurnSoft.Testing.Web.Selenium.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9995e183-2fac-4001-be76-37d98f3142e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BurnSoft.Testing.Web.Selenium.UnitTest/Settings/VS2019.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Testing.Web.Selenium.UnitTest.Settings
+{
+    public class VS2019
+    {
+        private static List<Tuple<string, string>> GeneralSettings()
+        {
+            List<Tuple<string,string>> ls = new List<Tuple<string, string>>();
+            ls.Add(new Tuple<string, string>("url_main","http://www.burnsoft.test"));
+            ls.Add(new Tuple<string, string>("url_login", "http://www.burnsoft.test/login.aspx"));
+            ls.Add(new Tuple<string, string>("UsrLogin", ""));
+            ls.Add(new Tuple<string, string>("UsrPwd", ""));
+            ls.Add(new Tuple<string, string>("SettingsScreenShotLocation", $"{Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"ScreenShots")}"));
+            //ls.Add(new Tuple<string, string>("", ""));
+            //ls.Add(new Tuple<string, string>("", ""));
+            return ls;
+        }
+
+        private static string GetSettings(string value)
+        {
+            string sAns = @"";
+            List<Tuple<string,string>> ls = GeneralSettings();
+            foreach (Tuple<string, string> l in ls)
+            {
+                if (l.Item1.Equals(value))
+                {
+                    sAns = l.Item2;
+                    break;
+                }
+            }
+            return sAns;
+        }
+
+        public static string GetSetting(string value) => GetSettings(value);
+        //protected override int GetSetting(string value) => Convert.ToInt32(GetSettings(value));
+    }
+}
