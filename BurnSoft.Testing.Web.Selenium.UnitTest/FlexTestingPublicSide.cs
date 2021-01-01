2c0a7e31b08f4419de6091d953902558c39d1206
Joe M <bs.github@burnsoft.net>
2021-01-01 10:30:28 -0500

    Started unit test for flex testing

---
+++ BurnSoft.Testing.Web.Selenium.UnitTest/FlexTestingPublicSide.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BurnSoft.Testing.Web.Selenium.UnitTest
+{
+    [TestClass]
+    public class FlexTestingPublicSide
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
