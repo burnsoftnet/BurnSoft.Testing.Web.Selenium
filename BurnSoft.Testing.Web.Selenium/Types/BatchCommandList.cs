e93270e8545e8eb1d6de762bf934094b565d9137
Joe M <bs.github@burnsoft.net>
2021-02-01 17:55:50 -0500

    #4 Added List type for batch function

--- BurnSoft.Testing.Web.Selenium/Types/BatchCommandList.cs
+++ BurnSoft.Testing.Web.Selenium/Types/BatchCommandList.cs
@@ -8,6 +8,14 @@ namespace BurnSoft.Testing.Web.Selenium.Types
 {
     public class BatchCommandList
     {
-        //public GeneralActions.FindBy
+        public GeneralActions.FindBy FindBy { get; set; }
+        public GeneralActions.MyAction Actions { get; set; }
+        public GeneralActions.UseCommand UseCommand { get; set; }
+        public string ElementName { get; set; }
+        public string SendKeys { get; set; }
+        public int SliderMoveTo { get; set; }
+        public int SliderMax { get; set; }
+        public int SliderMin { get; set; }
+
     }
 }
