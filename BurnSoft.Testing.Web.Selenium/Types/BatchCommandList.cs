05d60a8e3dc2994f63330a25e3b7e0241fa43d7e
Joe M <bs.github@burnsoft.net>
2021-02-01 17:59:44 -0500

    #4 Added Comments to list.

--- BurnSoft.Testing.Web.Selenium/Types/BatchCommandList.cs
+++ BurnSoft.Testing.Web.Selenium/Types/BatchCommandList.cs
@@ -1,20 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace BurnSoft.Testing.Web.Selenium.Types
 {
+    /// <summary>
+    /// Batch Command List to Run a List of Selnium commands and hold all the elements , values and commands
+    /// </summary>
     public class BatchCommandList
     {
+        /// <summary>
+        /// Fing By command, find by id, xpath, css, etc
+        /// </summary>
         public GeneralActions.FindBy FindBy { get; set; }
+        /// <summary>
+        /// Action to perform, send command, click, nothing, etc.
+        /// </summary>
         public GeneralActions.MyAction Actions { get; set; }
+        /// <summary>
+        /// Find, wait till found, wait, etc, performs the function to run
+        /// </summary>
         public GeneralActions.UseCommand UseCommand { get; set; }
+        /// <summary>
+        /// Element Name
+        /// </summary>
         public string ElementName { get; set; }
+        /// <summary>
+        /// Keys or text to send 
+        /// </summary>
         public string SendKeys { get; set; }
+        /// <summary>
+        /// Move slide x positions over + up, - down
+        /// </summary>
         public int SliderMoveTo { get; set; }
+        /// <summary>
+        /// The slider max value
+        /// </summary>
         public int SliderMax { get; set; }
+        /// <summary>
+        /// The slider min value.
+        /// </summary>
         public int SliderMin { get; set; }
 
     }
