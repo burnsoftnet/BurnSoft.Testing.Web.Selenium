20ca66b088578c21aa48c169d279e72be9c6f193
Joe M <bs.github@burnsoft.net>
2021-07-26 17:21:03 -0400

    #8 updated command helper class to unify test that isn't working

--- BurnSoft.Testing.Web.Selenium/BatchCmdHelper.cs
+++ BurnSoft.Testing.Web.Selenium/BatchCmdHelper.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BurnSoft.Testing.Web.Selenium.Types;
+// ReSharper disable UnusedMember.Global
 
 namespace BurnSoft.Testing.Web.Selenium
 {
-    class BatchCmdHelper
+    /// <summary>
+    /// Class BatchCmdHelper. A simpified verstion to add commands to the batch command list
+    /// </summary>
+    public class BatchCmdHelper
     {
+        /// <summary>
+        /// Clicks the on element., Just pass the basic name and the verification option switch will
+        /// tag on Click on or Verify to the main test name, by default we are looking for xpath, but you can change that.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="fb">The fb.</param>
+        /// <param name="verify">if set to <c>true</c> [verify].</param>
+        /// <returns>List&lt;BatchCommandList&gt;.</returns>
+        public static List<BatchCommandList> ClickOnElement(string element, string name,
+            GeneralActions.FindBy fb = GeneralActions.FindBy.XPath, bool verify = false)
+        {
+            List<BatchCommandList> cmd = new List<BatchCommandList>();
+            GeneralActions.MyAction action = verify ? GeneralActions.MyAction.Nothing : GeneralActions.MyAction.Click;
+            string testName = verify ? $"Verify {name}" : $"Click On {name}";
+            cmd.Add(new BatchCommandList()
+            {
+                Actions = action,
+                UseCommand = GeneralActions.UseCommand.WaitFound,
+                ElementName = element,
+                FindBy = fb,
+                TestName = testName
+            });
+            return cmd;
+        }
+        /// <summary>
+        /// Sets the text box.Just pass the basic name and the verification option switch will
+        /// tag on Click on or Verify to the main test name, by default we are looking for xpath, but you can change that.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="newValue">The new value.</param>
+        /// <param name="fb">The fb.</param>
+        /// <param name="verify">if set to <c>true</c> [verify].</param>
+        /// <returns>List&lt;BatchCommandList&gt;.</returns>
+        public static List<BatchCommandList> SetTextBox(string element, string name,string newValue,
+            GeneralActions.FindBy fb = GeneralActions.FindBy.XPath, bool verify = false)
+        {
+            List<BatchCommandList> cmd = new List<BatchCommandList>();
+            GeneralActions.MyAction action = verify ? GeneralActions.MyAction.Nothing : GeneralActions.MyAction.Click;
+            string testName = verify ? $"Verify {name}" : $"Set {name} to {newValue}";
+            cmd.Add(new BatchCommandList()
+            {
+                FindBy = fb,
+                Actions = action,
+                TestName = testName,
+                ElementName = element,
+                UseCommand = GeneralActions.UseCommand.WaitFound,
+                SendKeys = newValue
+
+            });
+            return cmd;
+        }
     }
 }
