8a4baf6d09e1b254ae2963f21443d6de54e4003d
Joe M <bs.github@burnsoft.net>
2021-11-03 15:54:46 -0400

    started Unit test for .net 5 unit test

---
+++ BurnSoft.Testing.Web.Selenium.Ns.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BurnSoft.Testing.Web.Selenium.Ns.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
