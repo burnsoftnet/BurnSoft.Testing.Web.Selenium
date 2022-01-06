b6f885b8bbe2472aeb8cc1aa6162fc7a2252bd91
Joe M <bs.github@burnsoft.net>
2022-01-06 11:05:47 -0500

    #12  Updated version

--- BurnSoft.Testing.Web.Selenium/Properties/AssemblyInfo.cs
+++ BurnSoft.Testing.Web.Selenium/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Testing.Web.Selenium")]
-[assembly: AssemblyCopyright("Copyright © www.burnsoft.net  2020-2021")]
+[assembly: AssemblyCopyright("Copyright © www.burnsoft.net  2020-2022")]
 [assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.36")]
-[assembly: AssemblyFileVersion("1.0.0.36")]
+[assembly: AssemblyVersion("1.0.0.37")]
+[assembly: AssemblyFileVersion("1.0.0.37")]
