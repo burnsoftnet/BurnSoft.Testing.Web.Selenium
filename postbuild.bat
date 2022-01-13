b751e1d2f4778e625a88d561e2dd9bdb45a3af0b
Joe M <bs.github@burnsoft.net>
2022-01-13 14:57:48 -0500

    #13 updated build process and inc version

--- postbuild.bat
+++ postbuild.bat
@@ -4,11 +4,18 @@ REM * VS Buid Script when using GhostScript for chm file *
 REM * and building nuget packages from Solution 	 *
 REM ******************************************************
 REM ******************************************************
-REM USE Build Script below for Solution
+REM USE Build Script below for Solution and upload to local server
 REM $(SolutionDir)postbuild.bat $(SolutionDir) $(ProjectDir) $(ConfigurationName) $(ProjectName)
+REM
+REM USE Build Script below for Solution and upload to GitHub
+REM $(SolutionDir)postbuild.bat $(SolutionDir) $(ProjectDir) $(ConfigurationName) $(ProjectName) -g
 REM 
-REM USE Build Script below for Project Only
+REM USE Build Script below for Project Only and upload to local server
 REM $(ProjectDir)postbuild.bat $(SolutionDir) $(ProjectDir) $(ConfigurationName) $(ProjectName)
+REM 
+REM USE Build Script below for Project Only and upload to GitHub
+REM $(ProjectDir)postbuild.bat $(SolutionDir) $(ProjectDir) $(ConfigurationName) $(ProjectName) -g
+
 REM ******************************************************
 SET SolutionDir=%1
 SET ProjectDir=%2
@@ -22,16 +29,16 @@ cd "%ProjectDir%"
 copy /Y "%SolutionDir%Help\%HELPFILENAME%.chm" "%ProjectDir%bin\%ConfigurationName%\%HELPFILENAME%.chm"
 cd "%ProjectDir%"
 del /Q %ProjectDir%*.nupkg
+SET LOCALNUGET=nuget.burnsoft.prod
+SET USENUGETSERVER="http://%LOCALNUGET%"
 
 if "%ConfigurationName%" == %DEBUG% (
 	echo "nuget Dev packing"
-	SET USENUGETSERVER="http://nuget.burnsoft.prod"
 	nuget pack
 )
 
 if "%ConfigurationName%" == %RELEASE% (
 	echo "nuget Production Packing"
-	SET USENUGETSERVER="http://nuget.burnsoft.prod"
 	nuget pack -Prop Configuration=Release
 )
 
@@ -46,12 +53,21 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`%SolutionDir%StringReplace %nupak% %HELPFILE
 	SET ver=%%F
 )
 echo "PackageVersion: %ver%"
+
+SET targetstatus=
+for /f "skip=3 tokens=6" %%g in ('ping %LOCALNUGET%^|find /i "TTL"') do @set targetstatus=%%g
+if "%targetstatus%"=="" echo %LOCALNUGET% is not reachable & goto GITHUB
+
 echo "deleting %HELPFILENAME% %ver%"
 nuget delete %HELPFILENAME% %ver% burnsoft -Source %USENUGETSERVER% -NonInteractive
 echo "Uploading %nupak%.%NUGETEXT%"
 nuget push %nupak%.%NUGETEXT% burnsoft -Source %USENUGETSERVER%
 
+:GITHUB
+
+if NOT "%~5"=="-g" goto END
 echo "nuget guthub push"
 nuget push %nupak%.%NUGETEXT% -source "github"
 
+:END
 cd ..
