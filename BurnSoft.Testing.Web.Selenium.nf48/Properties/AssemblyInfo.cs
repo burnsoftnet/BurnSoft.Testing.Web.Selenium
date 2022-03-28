81e3dd5538a63e5f34bd247a4922a237b5d830ed
burnsoftnet <bs.github@burnsoft.net>
2022-03-28 08:40:13 -0400

    #14 Updated project details

--- BurnSoft.Testing.Web.Selenium.nf48/Properties/AssemblyInfo.cs
+++ BurnSoft.Testing.Web.Selenium.nf48/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BurnSoft.Testing.Web.Selenium.nf48")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BurnSoft.Testing.Web.Selenium")]
+[assembly: AssemblyDescription("Web Testing library using Selenium to test any of the websites, just simplifying the process to to a unit test on your website")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Testing.Web.Selenium.nf48")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © www.burnsoft.net  2020-2022")]
+[assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.40")]
+[assembly: AssemblyFileVersion("1.0.0.40")]
